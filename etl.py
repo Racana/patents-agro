65bfa05088ffd012778474beddfcd8115087f0c9
Racana <pablo.racana@gmail.com>
2022-03-16 19:38:26 -0300

    changes made to etl

--- etl.py
+++ etl.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import json, os, sys, gzip
+import json, os, sys, gzip, csv
 from google.cloud import storage
 from tqdm import tqdm
 from datetime import datetime
+import re
+
 
 client = storage.Client()
 BUCKET_NAME = 'patents-agro' 
@@ -14,6 +16,7 @@ def list_blobs(client, bucket_name):
     blobs = list(blobs)
 
     return blobs
+
 
 def download_blobs(blobs, download_folder):
     filenames = []
@@ -29,6 +32,7 @@ def download_blobs(blobs, download_folder):
 
     return filenames
 
+
 def read_json(filename):
     gz =  gzip.open(filename)
     file = gz.read()
@@ -36,42 +40,31 @@ def read_json(filename):
 
     return json_data
 
-def extract_patent_text(json_data, patent_folder):
+
+def extract_patent_text(json_data, concat_patent):
     """Extract patent text data from jsonfile"""
-    patent = json_data[0]
     for patent in json_data:
         publication_number = patent['publication_number']
         try:
             title = patent['title_localized'][0]['text']
+            title = re.sub('\s+', ' ', title)
         except IndexError:
-            title = ""
+            continue
         try:
             abstract = patent['abstract_localized'][0]['text']
+            abstract = re.sub('\s+', ' ', abstract)
         except IndexError:
-            abstract = ""
+            continue
         try:
             claims = patent['claims_localized'][0]['text']
+            claims = re.sub('\s+', ' ', claims)
         except IndexError:
-            claims = ""
-        try:
-            description = patent['description_localized'][0]['text']
-        except IndexError:
-            description = ""
+            continue
 
-        patent_text = "\n".join([title, abstract, claims, description])
-
-        save_patent_text(publication_number, patent_text, patent_folder)
+        concat_patent.append([title, abstract, claims])
             
-    return None
+    return concat_patent
 
-def save_patent_text(publication_number, text, patent_folder):
-    text_filename = os.path.join(patent_folder, publication_number + ".txt")
-
-    text_file = open(text_filename, "wt", encoding="utf-8")
-    n = text_file.write(text)
-    text_file.close()
-
-    return None
 
 def extract_patent_metadata(patents_json, patents_metadata):
     for patent in patents_json:
@@ -94,8 +87,19 @@ def extract_patent_metadata(patents_json, patents_metadata):
         
         try:
             title = patent['title_localized'][0]['text']
+            title = re.sub('\s+', ' ', title)
         except IndexError:
-            title = None
+            continue
+        try:
+            abstract = patent['abstract_localized'][0]['text']
+            abstract = re.sub('\s+', ' ', abstract)
+        except IndexError:
+            continue
+        try:
+            claims = patent['claims_localized'][0]['text']
+            claims = re.sub('\s+', ' ', claims)
+        except IndexError:
+            continue
 
         for inventor in patent['inventor_harmonized']:
             inventor_list.append(inventor['name'])
@@ -125,6 +129,7 @@ def extract_patent_metadata(patents_json, patents_metadata):
 
     return patents_metadata
 
+
 def metadata_dataframe(patents_metadata):
     colnames_metadata = ['publication_number', 'country_code', 'title', 'publication_date', 
                 'filing_date', 'grant_date', 'inventor_list', 'inventor_list_cc', 
@@ -134,16 +139,28 @@ def metadata_dataframe(patents_metadata):
 
     return metadata
 
-def etl(download_folder, patent_folder):
+
+def save_concat_patent(concat_patent):
+    with open("concat_patent.csv", "w", newline="", encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerows(concat_patent)
+
+        return None
+
+
+def etl(download_folder):
     patents_metadata = []
+    concat_patent = [['title', 'abstract', 'claims']]
     blobs = list_blobs(client, BUCKET_NAME)
     filenames = download_blobs(blobs, download_folder)
     for json_file in filenames:
         json_data = read_json(json_file)
-        extract_patent_text(json_data, patent_folder)
+        concat_patent = extract_patent_text(json_data, concat_patent)
         patents_metadata = extract_patent_metadata(json_data, patents_metadata)
     
     metadata = metadata_dataframe(patents_metadata)
+
+    save_concat_patent(concat_patent)
     metadata.to_pickle(os.path.join(folder, "metadata.pkl"))
 
     return None
@@ -152,18 +169,13 @@ def etl(download_folder, patent_folder):
 if __name__ == '__main__':
 
     start = datetime.now() 
-    script, folder = sys.argv
-    download_folder = "download"
-    patent_folder = "patent"
-    download_folder = os.path.join(folder, download_folder)
-    patent_folder = os.path.join(folder, patent_folder)
+    folder = os.getcwd()    
+    download_folder = os.path.join(folder, "download")
 
     if not os.path.exists(download_folder):
         os.mkdir(download_folder)
-    
-    if not os.path.exists(patent_folder):
-        os.mkdir(patent_folder)
 
-    etl(download_folder, patent_folder)
+
+    etl(download_folder)
 
     print(datetime.now()-start)
